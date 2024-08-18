b550f490de55ecdfd58b8f1ba376f31e5491889c
marciodavid <marciodavid81@gmail.com>
2024-08-18 18:51:46 -0300

    fix: error heigth size

--- src/pages/Cart/styles.jsx
+++ src/pages/Cart/styles.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 export const StyledCart = styled.div`
     height: auto;
+    min-height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
