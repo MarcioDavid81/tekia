705769db142c6066c9a8695514a114a41faa7ba5
marciodavid <marciodavid81@gmail.com>
2024-09-13 20:23:39 -0300

    feat: add getfullyear

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,13 +2,14 @@ import { StyleFooter } from "./styles";
 
 function Footer() {
 
+    const ano = new Date().getFullYear();
 
 
     return (
         <StyleFooter>
             <div className="container">
                 <div className="copy">
-                    <p>&copy; 2024 - Todos os direitos reservados</p>
+                    <p>&copy; {ano} - Todos os direitos reservados</p>
                 </div>
                 <div className="developer">
                     <p>Powered by | <a href="https://futurodev-marcio.netlify.app/" target="_blank">Marcio David - Web Developer</a></p>
